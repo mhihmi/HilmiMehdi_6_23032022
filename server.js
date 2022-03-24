5637bcb048243fbd85f2180dd93cdfb56a31e963
mhihmi <mehdihilmi@gmail.com>
2022-03-24 12:53:56 +0100

    App link to NodeServer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Here is the server Response !');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
