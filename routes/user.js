8f3fc73fd662d5c301af555ca8c411a60d2c038b
mhihmi <mehdihilmi@gmail.com>
2022-04-03 14:28:38 +0200

    Email Validator Middleware & Route - Exam Files

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
-const password = require('../middleware/password');
+const pswCtrl = require('../middleware/password');
 const emailCtrl = require('../middleware/email');
 
 // Front send infos => routes POST
-router.post('/signup', password, userCtrl.signup);
+router.post('/signup', emailCtrl, pswCtrl, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
